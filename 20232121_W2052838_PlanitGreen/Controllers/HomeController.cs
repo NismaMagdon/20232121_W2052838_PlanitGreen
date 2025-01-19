84989949a5bcd259270cb20a0728a36465e91827
User <User@DESKTOP-H28KTVM>
2025-01-19 13:57:15 +0530

    Add project files.

---
+++ 20232121_W2052838_PlanitGreen/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using _20232121_W2052838_PlanitGreen.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _20232121_W2052838_PlanitGreen.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 20232121_W2052838_PlanitGreen/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _20232121_W2052838_PlanitGreen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
