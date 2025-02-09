663bf9019eead579a19dd4f3df4b16161f5c11e3
NismaMagdon <User@DESKTOP-H28KTVM>
2025-02-09 15:10:02 +0530

    DbContext class

---
+++ 20232121_W2052838_PlanitGreen/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _20232121_W2052838_PlanitGreen.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+    }
+}
