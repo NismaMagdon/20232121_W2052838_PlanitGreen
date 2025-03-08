3200229a6a615feb42eed72f8857e6af86b6dc3f
NismaMagdon <User@DESKTOP-H28KTVM>
2025-03-08 19:17:55 +0530

    8/3/2025 - Eco points calculation logic

---
+++ 20232121_W2052838_PlanitGreen/Managers/GamificationManager.cs
@@ -0,0 +1,10 @@
+﻿using _20232121_W2052838_PlanitGreen.Models;
+using System.Security.Cryptography;
+
+namespace _20232121_W2052838_PlanitGreen.Managers
+{
+    public class GamificationManager
+    {
+
+    }
+}
--- 20232121_W2052838_PlanitGreen/Models/Tour.cs
+++ 20232121_W2052838_PlanitGreen/Models/Tour.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using _20232121_W2052838_PlanitGreen.Managers;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace _20232121_W2052838_PlanitGreen.Models
@@ -14,7 +15,7 @@ namespace _20232121_W2052838_PlanitGreen.Models
         public virtual Destination Destination { get; set; }
         public int Duration { get; set; }
         public double Price { get; set; }
-        public string CarbonFootprint { get; set; }
+        public double CarbonFootprint { get; set; }
         public int TreesPlanted { get; set; }
         public bool IsActive { get; set; }
 
@@ -26,6 +27,41 @@ namespace _20232121_W2052838_PlanitGreen.Models
         public List<Review> Reviews { get; set; } = new List<Review>();
         
         public List<Departure> DepartureList { get; set; } = new List<Departure>();
+
+
+        public int CalculateEcoPoints()
+        {
+            int baseEcoPoints = 100; // Every tour gets at least 50 points
+
+            // Factor 1: Carbon Footprint Reduction
+            double averageCarbonFootprint = 100.0; // Assume an avg carbon footprint of 50kg
+
+            if ((averageCarbonFootprint - this.CarbonFootprint) > 0)
+            {
+                baseEcoPoints += (int)(averageCarbonFootprint - this.CarbonFootprint);
+            }
+
+            //Factor 2: Tour duration
+            if (this.Duration > 7) // If the trip duration is more than 7 days, add points
+            {
+                baseEcoPoints += (this.Duration - 7) * 2;
+            }
+
+            //Factor 3: Tree planting
+            baseEcoPoints += this.TreesPlanted * 5; //5 points per tree planted
+
+            //Factor 4: Tour style
+            switch(this.TourStyle.TourStyleName)
+            {
+
+                case "Leisure":
+                case "Adventure":
+                    baseEcoPoints -= 10;
+                    break;
+            }
+
+            return baseEcoPoints;
+        }
 
 
     }
