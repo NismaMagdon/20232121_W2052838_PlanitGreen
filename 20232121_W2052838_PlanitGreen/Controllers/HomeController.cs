25c032663293409625ff0ffcc50b601a4d595e73
NismaMagdon <User@DESKTOP-H28KTVM>
2025-04-11 23:39:32 +0530

    11/4/2025 - Eco rewards page

--- 20232121_W2052838_PlanitGreen/Controllers/HomeController.cs
+++ 20232121_W2052838_PlanitGreen/Controllers/HomeController.cs
@@ -1,16 +1,20 @@
 using System.Diagnostics;
+using _20232121_W2052838_PlanitGreen.Data;
 using _20232121_W2052838_PlanitGreen.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace _20232121_W2052838_PlanitGreen.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -25,7 +29,8 @@ namespace _20232121_W2052838_PlanitGreen.Controllers
 
         public IActionResult Rewards()
         {
-            return View();
+            var badges = _context.Badge.ToList();
+            return View(badges);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
